86157138736934479212d01607d0fc88f2cfc0f0
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-16 03:35:26 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,13 @@
+window.routerBase = "https://ss.shy521.com/";
+
+window.settings = {
+  title: "Xboard Apple Pink",
+  description: "Elegant & Soft Apple Pink Theme",
+  assets_path: "/assets",
+  theme: {
+    color: "default",
+  },
+  version: "1.0.0",
+  background_url: "",
+  logo: "",
+};
